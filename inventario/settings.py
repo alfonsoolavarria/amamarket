7e355fa8214bab990bae34532f76f21c68dce476
alfonso-dev <alfonso.olavarria@indagia.com>
2020-11-25 14:41:54 -0400

    debug fale

--- inventario/settings.py
+++ inventario/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'cpny$7u#mrpufw&pso4*@!v19098%f$ry3cnbj*#6h1q2wu=f%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','*']
 
